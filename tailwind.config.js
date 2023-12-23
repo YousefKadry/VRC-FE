340fb0ae8bce0a217cefecb10928deebd466c99e
Fady Emad <60940882+fady2019@users.noreply.github.com>
2023-12-23 18:22:29 +0200

    "chore: add needed dependencies and setup tailwind"

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    darkMode: 'class',
+    content: ['./src/**/*.{js,jsx,ts,tsx}', './node_modules/react-tailwindcss-select/dist/index.esm.js'],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+};
