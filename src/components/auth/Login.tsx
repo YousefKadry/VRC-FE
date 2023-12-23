a2e607334de42a9b65370b83731906a95985b8bd
Fady Emad <60940882+fady2019@users.noreply.github.com>
2023-12-23 18:26:21 +0200

    "chore: initialize routes setup and auth components"

---
+++ src/components/auth/Login.tsx
@@ -0,0 +1,5 @@
+const Login = () => {
+    return <h1 className="text-cyan-900 font-bold text-3xl">Login Works</h1>;
+};
+
+export default Login;
---
+++ src/components/auth/SignUp.tsx
@@ -0,0 +1,5 @@
+const SignUp = () => {
+    return <h1 className="text-cyan-900 font-bold text-3xl">SignUp Works</h1>;
+};
+
+export default SignUp;
