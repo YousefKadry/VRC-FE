31a2b5111e8448ad7a30bf79b1248e8547650ae1
Fady Emad <60940882+fady2019@users.noreply.github.com>
2024-01-06 12:53:23 +0200

    "refactor: refactor spinner component"

--- src/components/ui/spinner/Spinner.tsx
+++ src/components/ui/spinner/Spinner.tsx
@@ -12,19 +12,20 @@ const Spinner: React.FC<{
     cssOverride?: React.CSSProperties;
     speedMultiplier?: number;
 }> = (props) => {
-    const { ...restProps } = props;
+    if (!props.loading) {
+        return null;
+    }
 
     return (
         <>
             <Backdrop show={props.loading} />
 
-            {props.loading &&
-                ReactDOM.createPortal(
-                    <div className=" fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1001]">
-                        <HashLoader size="100px" color="#9d5ce9" {...restProps} />
-                    </div>,
-                    document.getElementById('spinner')!
-                )}
+            {ReactDOM.createPortal(
+                <div className=" fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1001]">
+                    <HashLoader size="100px" color="#9d5ce9" {...props} />
+                </div>,
+                document.getElementById('spinner')!
+            )}
         </>
     );
 };
