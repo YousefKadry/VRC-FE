a5fbeb4719783872d660604e3e5f625dbe5dfd67
Yousef Kadry <yousefkadry01@gmail.com>
2024-01-10 00:26:39 +0200

    draft [feat]: adding leva UI controller

--- src/components/simulation-room/SimulationRoom.tsx
+++ src/components/simulation-room/SimulationRoom.tsx
@@ -1,8 +1,23 @@
 import Sidebar from '../shared/Sidebar/Sidebar';
+import Controller from './transformation-controller/controller';
+import { Leva } from 'leva';scroll
 
 const SimulationRoom = () => {
+    const [transformations, mouseController] = Controller();
     return (
         <>
+            <div 
+                style={{
+                width: 320,
+                position: "absolute",
+                right: 0,
+                top: 0,
+                zIndex: 100,
+                opacity: 0.8
+                }}
+            >
+                <Leva fill />
+            </div>
             <Sidebar />
         </>
     );
