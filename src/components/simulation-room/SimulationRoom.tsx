faac62f7f0e0eebc13e84ac1eb121e3718028c18
Yousef Kadry <yousefkadry01@gmail.com>
2024-01-16 18:41:07 +0200

    leva ui integration with room store

--- src/components/simulation-room/SimulationRoom.tsx
+++ src/components/simulation-room/SimulationRoom.tsx
@@ -1,26 +1,43 @@
 import Sidebar from '../shared/Sidebar/Sidebar';
-import Controller from './transformation-controller/controller';
-import { Leva } from 'leva';scroll
+import useController from './transformation-controller/useController';
+import { Leva } from 'leva';
+import { useSelector } from 'react-redux';
+import { IAppStore } from '../../models/app-store';
+import { TAppDispatch } from '../../store/app-store';
+import { useDispatch } from 'react-redux';
+import { storeRoomsSliceActions } from '../../store/slices/rooms/rooms-slice';
 
 const SimulationRoom = () => {
-    const [transformations, mouseController] = Controller();
+    const meshes = useSelector((store: IAppStore) => store.rooms.selectedRoom?.state.meshes);
+    const selectedMesheID = useSelector((store: IAppStore) => store.rooms.selectedRoom?.state.selectedMeshId);
+    const selectedMesh = meshes ? meshes[selectedMesheID!] : null;
+    const dispatch = useDispatch<TAppDispatch>();
+
+    const [transformations, mouseController] = useController({selectedMesh, transformationChangeHandler:(type, value)=>{
+        dispatch(storeRoomsSliceActions.updateSelectedMesh({[type]:value}))      
+    }});
+
+    
+
+
     return (
-        <>
-            <div 
-                style={{
-                width: 320,
-                position: "absolute",
-                right: 0,
-                top: 0,
-                zIndex: 100,
-                opacity: 0.8
-                }}
-            >
-                <Leva fill />
-            </div>
-            <Sidebar />
-        </>
-    );
+    <>
+        <div 
+            style={{
+            width: 320,
+            position: "absolute",
+            right: 0,
+            top: 0,
+            zIndex: 100,
+            opacity: 0.8
+            }}
+        >
+            <Leva fill hidden={!selectedMesh} />
+        </div>
+        <Sidebar />
+    </>
+    )
+        
 };
 
 export default SimulationRoom;
