3fd0345dedac9bfe8c340049fe159d3e953cf82a
Yousef Kadry <yousefkadry01@gmail.com>
2024-01-28 00:41:39 +0200

    draft testing HTTPS

--- src/components/simulation-room/space/Space.tsx
+++ src/components/simulation-room/space/Space.tsx
@@ -2,6 +2,7 @@ import { useRef } from 'react';
 import { useSelector } from 'react-redux';
 import { Canvas } from '@react-three/fiber';
 import { OrbitControls, Plane, Sky, Stars } from '@react-three/drei';
+import { ARButton, XR } from '@react-three/xr';
 
 import SpaceClouds from './clouds/Clouds';
 import Meshes from './meshes/Meshes';
@@ -24,25 +25,37 @@ const Space: React.FC<ISpaceProps> = (props) => {
     }
 
     return (
-        <Canvas style={{ height: '100vh', width: '100%' }} camera={{ position: [-10, 15, 20] }}>
-            <OrbitControls ref={orbitRef} />
+        <>
+            {/* <VRButton /> */}
+            <ARButton />
+            <Canvas style={{ height: '100vh', width: '100%' }} camera={{ position: [-10, 15, 20] }}>
+                <XR>
+                    <OrbitControls ref={orbitRef} />
 
-            <ambientLight intensity={Math.PI / 2} />
-            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+                    <ambientLight intensity={Math.PI / 2} />
+                    <spotLight
+                        position={[10, 10, 10]}
+                        angle={0.15}
+                        penumbra={1}
+                        decay={0}
+                        intensity={Math.PI}
+                    />
+                    <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
 
-            <Plane args={[10, 10, 10, 10]} rotation={[1.5 * Math.PI, 0, 0]} position={[0, 0, 0]}>
-                <meshStandardMaterial attach="material" color="#f9c74f" wireframe />
-            </Plane>
+                    <Plane args={[10, 10, 10, 10]} rotation={[1.5 * Math.PI, 0, 0]} position={[0, 0, 0]}>
+                        <meshStandardMaterial attach="material" color="#f9c74f" wireframe />
+                    </Plane>
 
-            <Sky />
-            {selectedRoom.state.stars && <Stars />}
-            <Meshes />
-            <SpaceClouds />
-            {editable && selectedRoom.state.selectedObjectInfo && (
-                <SelectedObjectTransformControls orbitRef={orbitRef} />
-            )}
-        </Canvas>
+                    <Sky />
+                    {selectedRoom.state.stars && <Stars />}
+                    <Meshes />
+                    <SpaceClouds />
+                    {editable && selectedRoom.state.selectedObjectInfo && (
+                        <SelectedObjectTransformControls orbitRef={orbitRef} />
+                    )}
+                </XR>
+            </Canvas>
+        </>
     );
 };
 
