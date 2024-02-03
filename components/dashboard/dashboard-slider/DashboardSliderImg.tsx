e3c3b7a6a1e04f4f6dbeebdfaf3e8cd3520d117f
mennaNaem <156508562+mennaNaem@users.noreply.github.com>
2024-02-03 13:27:54 +0300

    Delete components directory

--- components/dashboard/dashboard-slider/DashboardSliderImg.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const DashboardSliderImg: React.FC<{ imgSrc: string }> = ({ imgSrc }) => {
-    return (
-        <img className="block w-full aspect-square object-contain" src={imgSrc} alt="dashboard-enter-room" />
-    );
-};
-
-export default DashboardSliderImg;
--- components/simulation-room/share-room/shareRoom.tsx
+++
@@ -1,7 +0,0 @@
-import SharingComponent from './SharingComponent/SharingComponent';
-
-const ShareRoom = () => {
-    return <SharingComponent sharingURL={`${document.URL}/view`} RenderQR={true} />;
-};
-
-export default ShareRoom;
